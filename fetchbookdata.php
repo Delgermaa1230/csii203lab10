ecbd96ce18aa88d3eb9eb3ff901ab7e9d38f48e8
Delgermaa1230 <145009550+Delgermaa1230@users.noreply.github.com>
2023-11-23 00:07:28 +0800

    номнуудыг харуулах fetchbookdata

---
+++ fetchbookdata.php
@@ -0,0 +1,32 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "Delgermaa1";
+$dbname = "mystuffs";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT s.stuffId, s.stuffName, s.stuffBdate, s.stuffEdate, s.stuffPrice, 
+c.categoryFunction, c.categoryId, c.categoryName, 
+u.Location, u.stuffusage
+from stuffs s, categories c, stuffsusage u
+where s.stuffid=u.stuffid and u.categoryid=c.categoryid and c.categoryid=3";
+$result = $conn->query($sql);
+
+$data = [];
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+}
+
+$conn->close();
+
+header('Content-Type: application/json');
+echo json_encode($data);
+?>
